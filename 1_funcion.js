21ce782436bd8a6cc6d36e3df44ba29bb0caa088
Javier <165967126+JavGarin@users.noreply.github.com>
2024-08-12 11:33:11 -0400

    Add files via upload
    exercises from JS expression and arrow function
    desafio latam

---
+++ 1_funcion.js
@@ -0,0 +1,11 @@
+// declaración.
+
+function example(a, b, c){
+    return a+b+c
+    }
+
+// Transformando esta declaración de función a una función de expresión.
+
+const expresion = function(a, b, c) {
+    return a + b + c;
+};
---
+++ 2_arrow.js
@@ -0,0 +1,14 @@
+// función
+
+let suma = function(a, b){
+    return a + b
+}
+
+
+// Transformando la función a una arrow function de una línea.
+
+let sumar = (a, b) => a + b;
+
+
+
+
